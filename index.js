5bc14721ae15452642f104006ca3dddcb722515f
Thaddeus <tdronski@gmail.com>
2024-06-22 21:30:06 -0500

    finished game logic game sucssefully plays

--- index.js
+++ index.js
@@ -12,19 +12,46 @@ const getHumanChoice = () => {
     return userPrompt;
 }
 
-const humanScore = 0; 
-const computerScore = 0; 
+let humanScore = 0; 
+let computerScore = 0; 
 
 const playRound = (humanChoice, computerChoice) => {
-    return humanChoice + computerChoice;
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lose. ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}`;
+    }
+}  
+
+
+
+const playGame = () => {
+    humanScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i ++) {
+        const humanSelection = getHumanChoice(); 
+        const computerSelection = getComputerChoice();
+        let result = playRound(humanSelection, computerSelection);
+        console.log(result);
+    }
+
+    console.log(`Final Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!");
+    } else if (humanScore < computerScore) {
+        console.log("Sorry, you lost the game.")
+    } else {
+        console.log("this game is a tie!")
+    }
 }
 
-const humanSelection = getHumanChoice(); 
-const computerSelection = getComputerChoice();
-
-const testResult = playRound(humanSelection, computerSelection);
-
-
-
-
-console.log(testResult);
+playGame();
